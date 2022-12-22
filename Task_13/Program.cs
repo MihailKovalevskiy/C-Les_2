02bdb3b17ffd90a04634435b2a7c146eb8c72633
Mikhail.kovalevsckij@gmail.com <Mikhail.kovalevsckij@gmail.com>
2022-12-22 22:05:09 +0800

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число: ");
+char[] array = Console.ReadLine().ToCharArray();
+var arrayValue = string.Join("", array);
+if (array.Length <= 2) 
+{
+    Console.WriteLine("Третьей цифры нет!");
+} 
+else 
+    Console.WriteLine($"Третья цифра числа = {array[2]}");
